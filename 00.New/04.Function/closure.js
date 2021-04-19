465d1b8c40cd2c7313ee52b793d7ffb811a852cc
ericabyss <erica051746@naver.com>
2021-04-19 15:59:45 +0900

    Update closure.js

--- 00.New/04.Function/closure.js
+++ 00.New/04.Function/closure.js
@@ -12,13 +12,13 @@ function makeId() {
 
 let idFunc = makeId();
 
-console.log(idFunc());
-console.log(idFunc());
-console.log(idFunc());
+console.log(idFunc());//1
+console.log(idFunc());//2
+console.log(idFunc());//3
 
 // Error
 // console.log(lastId);
 
 let idFund2 = makeId();
-console.log(idFund2());
-console.log(idFund2());
+console.log(idFund2());//1
+console.log(idFund2());//2
